cefc1a976c5186f0bc897147e0c2cd48f1e89511
Stanley Mwinde <salvadorethorbide8@gmail.com>
2025-04-10 12:51:22 +0300

    final changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["images.unsplash.com", "cdn.discordapp.com"],
+  },
+
+  //suppress webpack warnings
+  webpack: (config) => {
+    config.module?.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
