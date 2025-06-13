8a51d1c9e928e2e8b6bfe4004a46e4e0dff2c344
CarolineGatwiri-12 <cgatwiri@dimetechgroup.com>
2025-06-13 11:29:14 +0300

    spakers update

--- utils/index.tsx
+++ utils/index.tsx
@@ -45,18 +45,18 @@ export const SpeakersData = [
     bio: "Claire is a statistician, data scientist and AI enthusiast currently based in Nairobi Kenya. She has acquired hands-on experience in the implementation of various AI and Data Science techniques to benefit businesses and clients across the world. Her current goal is to build AI driven systems that solve problems for her community, for Africa and for the world at large.",
   },
   {
-    name: "Clarissa Soehnchen",
+    name: "Dr. Clarissa Soehnchen",
     title: "Founder We!Masomo",
     subtitle: "Panel Discussion",
     imageUrl: "clarissa.jpg",
     bio: "Clarissa Söhnchen is a researcher and entrepreneur focused on leveraging AI to transform healthcare education. As the founder of We!Masomo, she developed a platform that uses AI-driven, culturally tailored content to improve sexual and reproductive health education. Her work addresses healthcare gaps, empowers underserved communities, and promotes global health equity through innovative digital solutions.",
   },
   {
-    name: "Monicah W.Muhoya",
+    name: "Monicah W. Muhoya",
     title: "Digital Programmes Coordinator UNITAR",
     subtitle: "Panel Discussion",
     imageUrl: "monicah.jpg",
-    bio: "Monicah is a social entrepreneur with a background in technology having worked in projects within telecom, start up and software industries. She is passionate about CSR strategy, social justice, the empowerment of women and girls, social entrepreneurship, gender tech, and innovation. She is the founder of SisterSpeaks Global, a female-centered organization that curates and hosts safe spaces and events to increase women's dialogue and networking capacities. She established the #Heels4Pads Foundation, a charitable organization that raises period poverty awareness through creative campaigns, advocacy, education, and menstrual health innovation. Additionally, she works as a research associate in public learning institutions in Kenya supporting menstrual health and SRHR to identify and address gaps in access to and quality of SRHR through digital and social media. As a #OVAReact podcast host, she covers topics of navigating life with ovaries highlighting barriers and progress women have made in education, career, finance, innovation amd tech as well as leadership to advance gender equality. # As a tech/innovation enthusiast she serves on Hackathon Jury teams globally. Developing, planning and collaborating to solve the world's most challenging barriers t that hinder women's and girls' wellbein through hackathons.",
+    bio: "Monicah is a social entrepreneur with a background in technology having worked in projects within telecom, start up and software industries. She is passionate about CSR strategy, social justice, the empowerment of women and girls, social entrepreneurship, gender tech, and innovation. She is the founder of SisterSpeaks Global, a female-centered organization that curates and hosts safe spaces and events to increase women's dialogue and networking capacities. She established the #Heels4Pads Foundation, a charitable organization that raises period poverty awareness through creative campaigns, advocacy, education, and menstrual health innovation. Additionally, she works as a research associate in public learning institutions in Kenya supporting menstrual health and SRHR to identify and address gaps in access to and quality of SRHR through digital and social media. As a #OVAReact podcast host, she covers topics of navigating life with ovaries highlighting barriers and progress women have made in education, career, finance, innovation amd tech as well as leadership to advance gender equality. As a tech/innovation enthusiast she serves on Hackathon Jury teams globally. Developing, planning and collaborating to solve the world's most challenging barriers that hinder women's and girls' wellbeing through hackathons.",
   },
   {
     name: "James Ayugi",
@@ -70,14 +70,14 @@ export const SpeakersData = [
     title: "Head of Digital Strategy",
     subtitle: "Keynote Speaker",
     imageUrl: "grace.jpg",
-    bio: "Grace Murugi is a Christian marketplace leader and an award winning digital strategist.She is reputed for linking digital campaigns to business growth, social impact and brand equity. With a keen eye for new market share, she offers a unique blend of leadership, vision and knowledge on holistic digital strategy and implementation.She worked at WPP Scangroup handling consumer packaged goods for Unilever and Kimberly Clark, and then transitioned to Safaricom where she was Digital Lead for close to 5 years. In this capacity, she championed the insourcing of digital media for Safaricom, as well as campaigns like Eliud 159, Mpesa App among many other campaigns. She then transitioned to Oxfam as the Head of Digital Strategy and has championed the influencing of humanitarian work on the global digital landscape. She is excited about the impact work that she is a part of delivering at Oxfam.Grace is also a seasoned entrepreneur. She founded a business, scaled it and then sold it last year. She is also a Top 40 under 40 Women in Kenya 2023 recipient.",
+    bio: "Grace Murugi is a Christian marketplace leader and an award-winning digital strategist. She is reputed for linking digital campaigns to business growth, social impact and brand equity. With a keen eye for new market share, she offers a unique blend of leadership, vision, and knowledge of holistic digital strategy and implementation. She worked at WPP Scangroup, handling consumer packaged goods for Unilever and Kimberly-Clark, and then transitioned to Safaricom, where she was Digital Lead for close to 5 years. In this capacity, she championed the insourcing of digital media for Safaricom, as well as campaigns like Eliud 159 and the M-Pesa App, among many other campaigns. She then transitioned to Oxfam as the Head of Digital Strategy and has championed the influence of humanitarian work on the global digital landscape. She is excited about the impact work that she is a part of delivering at Oxfam. Grace is also a seasoned entrepreneur. She founded a business, scaled it and then sold it last year. She is also a Top 40 under 40 Women in Kenya 2023 recipient. ",
   },
   {
     name: "Quinter Ochieng",
     title: "CEO at WECHARGE",
     subtitle: "Panel discussion",
     imageUrl: "Quinterrr.jpg",
-    bio: "QUINTER is the CEO & Director Charge24. Ochieng Quinter spearheads Charge24 Africa, a cutting-edge power bank rental and interactive digital advertising solution. More than just a tech startup, Charge24 is a movement redefining how Africa connects, powers, and advertises. In today’s digital landscape, data is crucial, and Charge24 provides data-driven advertising solutions not only for corporate brands but also for impact-focused organizations.She believes Africa doesn’t need to catch up—it needs to be different at Art for Change – Her passion for storytelling and artistic expression led to the establishment of Art for Change in 2018, creating a platform where creatives use art for advocacy and social transformation. Over the years, she has supported over 60 artists and led impactful initiatives, including mental health awareness campaigns, youth empowerment workshops, and gender equity advocacy programs, reaching thousands across Kenya.Her programs have also been hosted in Rwanda & Peru in partnership with Planned Parenthood,using art to address Sexual and Reproductive Health & Rights (SRHR). In Burundi, Art for Change has promoted peace through art while economically empowering artists by showcasing their work in art galleries in London, UK, in partnership with the Kenyan Government’s Ministry of Sports and Arts.Her work has been recognized globally with prestigious awards, including some but not all:🏆 Impact Organization of the Year – Female Founders Initiative, Middle East & Africa (FFMEA, Supported by UN Women) (2021)🏆 FortyUnder40 Africa - Arts Awardee (2023)",
+    bio: "Quinter is the CEO & Director of Charge24. Ochieng Quinter spearheads Charge24 Africa, a cutting-edge power bank rental and interactive digital advertising solution. More than just a tech startup, Charge24 is a movement redefining how Africa connects, powers, and advertises. In today’s digital landscape, data is crucial, and Charge24 provides data-driven advertising solutions not only for corporate brands but also for impact-focused organisations. She believes Africa doesn’t need to catch up—it needs to be different in Art for Change. Her passion for storytelling and artistic expression led to the establishment of Art for Change in 2018, creating a platform where creatives use art for advocacy and social transformation. Over the years, she has supported over 60 artists and led impactful initiatives, including mental health awareness campaigns, youth empowerment workshops, and gender equity advocacy programs, reaching thousands across Kenya. Her programs have also been hosted in Rwanda & Peru in partnership with Planned Parenthood, using art to address Sexual and Reproductive Health & Rights (SRHR). In Burundi, Art for Change has promoted peace through art while economically empowering artists by showcasing their work in art galleries in London, UK, in partnership with the Kenyan Government’s Ministry of Sports and Arts. Her work has been recognised globally with prestigious awards, including some but not all: Impact Organisation of the Year – Female Founders Initiative, Middle East & Africa (FFMEA, Supported by UN Women) (2021), FortyUnder40 Africa - Arts Awardee (2023)",
   },
   {
     name: "Melody Mukhwana",
@@ -87,7 +87,7 @@ export const SpeakersData = [
     bio: "Melody Mukhwana is the Founder & CEO of Humainly AI, a consultancy helping African businesses adopt human-centered AI tools that boost performance, reduce burnout, and build scalable systems with heart. With a background in counseling psychology and five years in AI strategy and execution, she bridges the gap between cutting-edge tech and real human needs. Melody is passionate about making AI more accessible, championing women in tech, and helping teams unlock intentional growth through bold strategy, empathy, and innovation that feels deeply human.",
   },
   {
-    name: "DR. Tayba Hatimy",
+    name: "Dr. Tayba Hatimy",
     title: "Co-Founder and Executive Director of Baus Taka",
     subtitle: "Panel Discussion",
     imageUrl: "tayba.webp",
@@ -98,11 +98,11 @@ export const SpeakersData = [
     title: "Executive Director- Solar Mtaani",
     subtitle: "Panel Discussion",
     imageUrl: "abigail.jpg",
-    bio: "Abigaill Okello is the Executive Director of Solar Mtaani, a social enterprise dedicated to providing clean, affordable, and sustainable solar energy solutions to underserved communities in Kenya. With a deep commitment to energy justice and community development, Abigaill leads the organization with a vision of lighting up lives—literally and figuratively—through accessible solar technology.Under her leadership, Solar Mtaani has focused on bridging the energy gap in informal settlements and rural areas, promoting not just energy access but also education, economic empowerment, and environmental sustainability. Abigaill's approach blends grassroots engagement with strategic partnerships, ensuring that the solutions Solar Mtaani delivers are inclusive, scalable, and community-driven.Her background in social impact and organizational leadership enables her to champion both operational excellence and social equity. She believes that every household deserves the dignity of reliable power, and she is passionate about ensuring that clean energy reaches the last mile.Abigaill is not only a leader but also an advocate—working at the intersection of innovation, advocacy, and community to transform the energy landscape from the ground up.",
+    bio: "Abigaill Okello is the Executive Director of Solar Mtaani, a social enterprise dedicated to providing clean, affordable, and sustainable solar energy solutions to underserved communities in Kenya. With a deep commitment to energy justice and community development, Abigaill leads the organisation with a vision of lighting up lives—literally and figuratively—through accessible solar technology. Under her leadership, Solar Mtaani has focused on bridging the energy gap in informal settlements and rural areas, promoting not just energy access but also education, economic empowerment, and environmental sustainability. Abigaill's approach blends grassroots engagement with strategic partnerships, ensuring that the solutions Solar Mtaani delivers are inclusive, scalable, and community-driven. Her background in social impact and organisational leadership enables her to champion both operational excellence and social equity. She believes that every household deserves the dignity of reliable power, and she is passionate about ensuring that clean energy reaches the last mile. Abigaill is not only a leader but also an advocate, working at the intersection of innovation, advocacy, and community to transform the energy landscape from the ground up.",
   },
   {
     name: "Radhika Bhachu",
-    title: "Founder Ndovu",
+    title: "Founder of Ndovu",
     subtitle: "Keynote Speaker",
     imageUrl: "rhadhika.webp",
     bio: "Radhika has over 14 years of international experience in the financial services industry where she advised clients on how to invest their savings at BlackRock (London) as a Relationship Director.",
@@ -112,7 +112,7 @@ export const SpeakersData = [
     title: "Cyber Security Engineer",
     subtitle: "Keynote Speaker",
     imageUrl: "bright.jpg",
-    bio: "Dr Bright Gameli Mawudor is a cybersecurity tsar, with a PhD (which he got at 28) in IT Convergence and Application Engineering with a concentration in Information Security from Pukyong National University, South Korea.Have things turned out as expected at his age? “Unexpectedly. For instance, I founded Africahackon nine years ago, the first-ever hands-on cyber security conference in East Africa that aims to bring awareness with live demonstrations of how hacking happens, which has since grown into a juggernaut, and it’s amazing to see children doing cybersecurity.",
+    bio: "Dr Bright Gameli Mawudor is a cybersecurity tsar, with a PhD (which he got at 28) in IT Convergence and Application Engineering with a concentration in Information Security from Pukyong National University, South Korea. Have things turned out as expected at his age? “Unexpectedly. For instance, I founded Africahackon nine years ago, the first-ever hands-on cybersecurity conference in East Africa that aims to bring awareness with live demonstrations of how hacking happens, which has since grown into a juggernaut, and it’s amazing to see children doing cybersecurity.",
   },
   {
     name: "Maureen Gitau",
@@ -127,6 +127,20 @@ export const SpeakersData = [
     subtitle: "Panel Discussion",
     imageUrl: "paulb.jpeg",
     bio: " Paul Brian is a Cybersecurity Engineer at Kenya Commercial Bank Group with proven expertise in security design and architecture, penetration testing, application security, DevSecOps, cloud security, data privacy, and data protection. He is the author of Parenting in the Digital Era, among other books, a writer, and a seasoned speaker on cybersecurity, internet safety, and digital governance, leadership and personal growth. Paul is also the Founder and Host of the Success Ladder Podcast, a global platform spotlighting powerful conversations on leadership, personal growth, entrepreneurship, and impactful success stories.",
+  },
+  {
+    name: "Susan Gitaru",
+    title: "Founder of Phoenix KE Analytics",
+    subtitle: "Panel Discussion",
+    imageUrl: "Susan_Gatura_photo.jpg",
+    bio: "Susan Gatura is a data leader, mentor, and advocate for nurturing the next generation of data professionals in Africa. She is the Founder of Phoenix KE Analytics, a thriving data community that empowers aspiring and established data professionals through mentorship, skills development, and networking opportunities.With a background in finance and a self-taught journey into data, Susan is currently a Senior Analytics Specialist at MultiChoice Group, where she supports data strategies across over 10 countries. Her impact extends beyond her corporate role-she has been recognized as a Top 40 Under 40 Woman in Kenya shortlisted as DataIQ Data Champion, a finalist for the WomenTech Global Community Award, and being included in the 50 Inspiring Women in Tech in Africa 2023 highlighting her contributions to the industry.Passionate about creating opportunities, Susan has spearheaded initiatives such as Phoenix Masterclasses, Mentorship programs, and Data Awareness Drives in Schools, fostering knowledge-sharing and career growth in tech. Committed to breaking barriers and increasing diversity in data, Susan continuously champions mentorship and innovation, proving that with the right community and support, anyone can thrive in the data space.",
+  },
+  {
+    name: "Catherine Jeruto",
+    title: "Cybersecurity leader and mentor",
+    subtitle: "Panel Discussion",
+    imageUrl: "Catherine.jpg",
+    bio: "Catherine Jeruto is a cybersecurity leader and mentor, acclaimed for her ability to create user-focused security awareness programs that help organizations worldwide turn technical risks into practical strategies. She blends academic knowledge with real-world experience to develop multilingual training, board-level cybersecurity frameworks, and risk-aware cultures, especially within financial and civil society sectors. Catherine has led large-scale security campaigns, reduced phishing risks via customized workshops, and directed vulnerability management teams to strengthen infrastructure. Recognized as one of the Top 100 Cybersecurity Women to Watch in Africa (2024), she also leads a working group at the Kenya Cybersecurity and Forensics Association and actively mentors, speaks at conferences, and forges partnerships to make cybersecurity accessible to all. Qualifications; Bsc. Computer Science, ISC2 Certified in Cybersecurity (CC), Information Technology Infrastructure Library (ITIL) v4, Certified Ethical Hacker (CEH).",
   },
 ];
 
